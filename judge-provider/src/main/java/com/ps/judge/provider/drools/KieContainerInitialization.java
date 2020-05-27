091615faadd4580ab0217a79a16a33f313832497
zyd0129 <zyd_0129@qq.com>
2020-05-27 15:28:08 +0800

    change file name

--- judge-provider/src/main/java/com/ps/judge/provider/drools/KieContainerInitialization.java
+++ judge-provider/src/main/java/com/ps/judge/provider/drools/KieContainerInitialization.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Component
 @Slf4j
-public class KieContainerInitiate {
+public class KieContainerInitialization {
     @Autowired
     ConfigFlowService configFlowService;
 
