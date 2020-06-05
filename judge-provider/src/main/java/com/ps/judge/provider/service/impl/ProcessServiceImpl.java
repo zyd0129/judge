1c31be64976df00179a0297eca98efca51c9b55f
9044 <zhangqian9044@adpanshi.com>
2020-06-05 09:30:32 +0800

    judge 解决循环依赖问题,部分代码优化

--- judge-provider/src/main/java/com/ps/judge/provider/service/impl/ProcessServiceImpl.java
+++ judge-provider/src/main/java/com/ps/judge/provider/service/impl/ProcessServiceImpl.java
@@ -21,6 +21,7 @@ import com.ps.jury.api.response.VarResult;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -47,6 +48,7 @@ public class ProcessServiceImpl implements ProcessService {
     @Autowired
     HttpHeaders httpHeaders;
     @Autowired
+    @Lazy
     AsyncProcessTask asyncProcessTask;
 
     @Override
