b97cd7a5c946f679910389fba183aa8629389b1d
9044 <zhangqian9044@adpanshi.com>
2020-06-28 18:29:44 +0800

    judge增加judge-rules模块

--- judge-provider/src/test/java/com/ps/judge/provider/JudgeProviderApplicationTests.java
+++ judge-provider/src/test/java/com/ps/judge/provider/JudgeProviderApplicationTests.java
@@ -1,13 +1,22 @@
 package com.ps.judge.provider;
 
+import com.ps.jury.api.JuryApi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JudgeProviderApplicationTests {
+    @Autowired
+    JuryApi juryApi;
+
     @Test
     public void contextLoads() {
+        System.err.println(this.juryApi.getVarResult("12", "12"));
     }
 }
