91dcbfea68537e7381e8fac4299a9458676d45d2
zyd0129 <zyd_0129@qq.com>
2020-07-06 10:41:02 +0800

    修改cors逻辑

--- judge-web/src/main/java/com/ps/judge/web/config/JudgeWebAutoConfiguration.java
+++ judge-web/src/main/java/com/ps/judge/web/config/JudgeWebAutoConfiguration.java
@@ -26,6 +26,10 @@ public class JudgeWebAutoConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
     }
 }
