da61e1277491df9fd425dcdaa284251cf6151d03
Administrator <gitlab@adpanshi.com>
2020-07-07 19:34:29 +0800

    添加CI部署Shell脚本

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/env bash
 #
-# 解析.ci/config.json文件并根据.ci/deployJarTemplate.sh生成部署shell脚本
+# 解析.ci/config.json文件并根据.ci/deploy_jar.sh生成部署shell脚本
 # 廖发友
 # 20200707
 
 config=".ci/config.json"
 template=".ci/deployJarTemplate.sh"
 upload_max_count=3
+store=$1
 
 python -c "import json;import sys;reload(sys);sys.setdefaultencoding('utf-8');f=open(\"${config}\");j=json.loads(f.read());print(j[\"app\"])"
 
@@ -41,10 +42,10 @@ function deploy() {
             sed -i "s#START_CMD#${start_cmd}#" ${temp_name}
             for (( uploadCount=0; uploadCount<${upload_max_count}; uploadCount++ ));do
                 echo -e "将文件 ${jar_name} 上传到 ${host_ip} 目录 /data/.tmp-gitlab-runner"
-                scp -B -C ${HOME}/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}/${jar_name} root@${host_ip}:/data/.tmp-gitlab-runner
+                scp -B -C ${store}/${jar_name} root@${host_ip}:/data/.tmp-gitlab-runner
                 echo -e "${jar_name} 开始进行文件md5校验"
                 remote_md5=$(ssh root@${host_ip} "cd /data/.tmp-gitlab-runner && md5sum ${jar_name}")
-                if ( fgrep -q ${remote_md5} ${HOME}/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}/md5.txt );then
+                if ( fgrep -q ${remote_md5} ${store}/md5.txt );then
                     echo "md5值相同"
                     break
                 else
@@ -68,9 +69,9 @@ function build() {
         check "${jar_name}" "jar_name" "${app_count}"
         prefix_name=$(echo ${jar_name} | grep -oP '(.*)(?=-(\d+\.)+(.*?)jar)')
         echo -e "转存 ${jar_name} --> ${HOME}/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}/${jar_name}"
-        cp -rf ${prefix_name}/target/${jar_name} ${HOME}/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}
+        cp -rf ${prefix_name}/target/${jar_name} ${store}
     done
-    cd ${HOME}/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}
+    cd ${store}
     for file in $(ls);do
         echo -e "生成文件\t${file}\t的md5值"
         md5sum $file >> md5.txt
